1a3ccc21e88cb78aac29c0b7aee7737bd9f098ed
dimohy <58620778+dimohy@users.noreply.github.com>
2022-06-21 11:57:23 +0900

    3. 마이그레이션

---
+++ 4. 마이그레이션/TodoApp/Entities/TodoChangedHistory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Entities
+{
+    [Table(nameof(TodoChangedHistory))]
+    public class TodoChangedHistory
+    {
+        // 복합 키
+        [ForeignKey(nameof(Todo))]
+        public int TodoSeq { get; set; }
+        public int Seq { get; set; }
+        //
+
+        public TodoChangedKind ChangedKind { get; set; }
+        public string Before { get; set; } = default!;
+        public string After { get; set; } = default!;
+
+        public TodoInfo Todo { get; set; } = default!;
+    }
+
+    public enum TodoChangedKind
+    {
+        할일날짜변경 = 1,
+        완료날짜변경 = 2,
+        완료유무변경 = 3,
+        메모변경 = 4,
+        삭제유무변경 = 5,
+    }
+}
---
+++ 4. 마이그레이션/TodoApp/Entities/TodoInfo.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApp.Entities;
+
+[Table(nameof(TodoInfo))]
+public record TodoInfo
+{
+    [Key]
+    public int Seq { get; set; }
+
+    public DateOnly? TodoDate { get; set; }
+    public DateOnly? CompleteDate { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Memo { get; set; }
+    public bool IsDel { get; set; }
+
+    [ForeignKey(nameof(User))]
+    public string UserId { get; set; } = default!;
+    
+    public virtual UserInfo User { get; set; } = default!;
+
+    public virtual ICollection<TodoChangedHistory> Histories { get; set; } = default!;
+    public virtual ICollection<TodoTagInfo> Tags { get; set; } = default!;
+}

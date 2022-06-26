dec086a8a2a6f8f3d9cb10a0adc0fbe2b7c053a9
dimohy <58620778+dimohy@users.noreply.github.com>
2022-06-27 01:20:32 +0900

    5. 변경 추적 기능

---
+++ 5. 변경 추적 기능/TodoApp/DbContexts/TodoContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TodoApp.Entities;
+
+namespace TodoApp.DbContexts;
+
+public class TodoContext : DbContext
+{
+    public DbSet<UserInfo> Users => Set<UserInfo>();
+    public DbSet<TodoInfo> Todos => Set<TodoInfo>();
+    public DbSet<TodoChangedHistory> TodoChangedHistories => Set<TodoChangedHistory>();
+    //public DbSet<TagInfo> Tags => Set<TagInfo>();
+    public DbSet<TodoTagInfo> TodoTags => Set<TodoTagInfo>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // "database.db" 파일로 SQLite 사용
+        optionsBuilder.UseSqlite("Data Source=database.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Todo의 `Seq`키는 자동 증가
+        modelBuilder.Entity<TodoInfo>()
+            .Property(x => x.Seq)
+            .ValueGeneratedOnAdd();
+
+        // TodoChangedHistory는 복합키(TodoSeq, Seq)
+        modelBuilder.Entity<TodoChangedHistory>()
+            .HasKey(x => new { x.TodoSeq, x.Seq });
+    }
+}
---
+++ 5. 변경 추적 기능/TodoApp/Migrations/20220621022016_220621.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Migrations
+{
+    public partial class _220621 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoChangedHistory",
+                columns: table => new
+                {
+                    TodoSeq = table.Column<int>(type: "INTEGER", nullable: false),
+                    Seq = table.Column<int>(type: "INTEGER", nullable: false),
+                    ChangedKind = table.Column<int>(type: "INTEGER", nullable: false),
+                    Before = table.Column<string>(type: "TEXT", nullable: false),
+                    After = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoChangedHistory", x => new { x.TodoSeq, x.Seq });
+                    table.ForeignKey(
+                        name: "FK_TodoChangedHistory_TodoInfo_TodoSeq",
+                        column: x => x.TodoSeq,
+                        principalTable: "TodoInfo",
+                        principalColumn: "Seq",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoTagInfo",
+                columns: table => new
+                {
+                    TagId = table.Column<string>(type: "TEXT", nullable: false),
+                    Descption = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoTagInfo", x => x.TagId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoInfoTodoTagInfo",
+                columns: table => new
+                {
+                    TagsTagId = table.Column<string>(type: "TEXT", nullable: false),
+                    TodosSeq = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoInfoTodoTagInfo", x => new { x.TagsTagId, x.TodosSeq });
+                    table.ForeignKey(
+                        name: "FK_TodoInfoTodoTagInfo_TodoInfo_TodosSeq",
+                        column: x => x.TodosSeq,
+                        principalTable: "TodoInfo",
+                        principalColumn: "Seq",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TodoInfoTodoTagInfo_TodoTagInfo_TagsTagId",
+                        column: x => x.TagsTagId,
+                        principalTable: "TodoTagInfo",
+                        principalColumn: "TagId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoInfoTodoTagInfo_TodosSeq",
+                table: "TodoInfoTodoTagInfo",
+                column: "TodosSeq");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoChangedHistory");
+
+            migrationBuilder.DropTable(
+                name: "TodoInfoTodoTagInfo");
+
+            migrationBuilder.DropTable(
+                name: "TodoTagInfo");
+        }
+    }
+}

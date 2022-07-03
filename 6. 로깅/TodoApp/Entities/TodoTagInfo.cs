3b419c5add64dd5a54bd2e69d61950d0cc98ecb3
dimohy <58620778+dimohy@users.noreply.github.com>
2022-07-03 20:18:32 +0900

    6. 로깅

---
+++ 6. 로깅/TodoApp/Entities/TodoTagInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Entities
+{
+    [Table(nameof(TodoTagInfo))]
+    public class TodoTagInfo
+    {
+        [Key]
+        public string TagId { get; set; } = default!;
+
+        public string Descption { get; set; } = "";
+
+        public virtual ICollection<TodoInfo> Todos { get; } = default!;
+    }
+}
---
+++ 6. 로깅/TodoApp/Program.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TodoApp.DbContexts;
+using TodoApp.Entities;
+
+using var c = new TodoContext();
+c.Database.Log
+
+/* 추가 {{{
+var newUser = new UserInfo
+{
+    UserId = "test",
+    UserName = "test"
+};
+
+c.Users.Add(newUser);
+
+Console.WriteLine(c.Entry(newUser).State);
+
+var result = c.SaveChanges();
+Console.WriteLine(result);
+
+Console.WriteLine(c.Entry(newUser).State);
+}}} */
+
+
+
+/* 변경 {{{
+var testUser = c.Users.Find("test")!;
+Console.WriteLine(testUser);
+
+Console.WriteLine(c.Entry(testUser).State);
+
+testUser.UserName = "테스트";
+
+Console.WriteLine(c.Entry(testUser).State);
+
+var result = c.SaveChanges();
+Console.WriteLine(result);
+
+Console.WriteLine(c.Entry(testUser).State);
+
+testUser = c.Users.Find("test")!;
+Console.WriteLine(testUser);
+*/
+
+
+/* 삭제 {{{
+var testUser = c.Users.Find("test")!;
+
+Console.WriteLine(c.Entry(testUser).State);
+
+c.Users.Remove(testUser);
+
+Console.WriteLine(c.Entry(testUser).State);
+
+var result = c.SaveChanges();
+Console.WriteLine(result);
+
+Console.WriteLine(c.Entry(testUser).State);
+
+testUser = c.Users.Find("test")!;
+Console.WriteLine(testUser);
+}}} */
+
+
+/* 탐색 속성 추적 확인 {{{
+var testUser = c.Users.Include(x => x.Todos).First(x => x.UserId == "test");
+var newTodo = new TodoInfo
+{
+    //UserId = "test",
+    TodoDate = DateOnly.FromDateTime(DateTime.Now),
+    IsComplete = false,
+    IsDel = false,
+    Memo = "탐색 속성 변경 추적 확인",
+    Tags = new TodoTagInfo[] { new() { TagId = "dev", Descption = "개발" }, new() { TagId = "test", Descption = "테스트" } }
+};
+
+testUser.Todos.Add(newTodo);
+
+var result = c.SaveChanges();
+Console.WriteLine(result);
+}}} */
+
+var testUser =  c.Users
+                .Include(user => user.Todos)
+                    .ThenInclude(todo => todo.Tags)
+                .First(x => x.UserId == "test");
+
+foreach (var testTodos in testUser.Todos)
+{
+    Console.WriteLine(testTodos.Memo);
+    foreach (var tag in testTodos.Tags)
+        Console.WriteLine($"Tags : {tag.TagId} - {tag.Descption}");
+}
